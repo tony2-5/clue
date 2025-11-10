23da9678ae39b0b7f277a3d07e05ef5fbeb18834
tony2-5 <antdvory@gmail.com>
2025-11-10 15:36:39 -0500

    Simple game loop setup

--- main.pl
+++ main.pl
@@ -1,7 +1,34 @@
 :- [gamesetup].
 
 % main game loop
+:- dynamic game_state/1.
 setupgame :-
+  retractall(game_state(_)),
   print_board,
   winning_cards,
-  distribute_cards.
+  distribute_cards,
+  assert(game_state(running)),
+  game_loop.
+
+game_loop :-
+  game_state(finished),
+  write('Game over!'), nl.
+game_loop :-
+  game_state(running),
+  guess -> retract(game_state(running)), assert(game_state(finished))
+  ; (write('incorrect guess'), nl),
+  game_loop.
+
+% Win check
+guess :-
+  write('Enter your guess: '), nl,
+  write('What weapon was used?: '), read(Weapon), nl,
+  write('Who commited the crime?: '), read(Character), nl,
+  write('Which room was the crime committed?: '), read(Room), nl,
+  winningcards(A),
+  [Weapon, Character, Room] = A.
+
+% Two die rolls between 1 and 12
+rolldice(Moves) :-
+  random_between(1, 12, Moves).
+  
